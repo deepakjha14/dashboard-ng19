971fea02c06c197114ab1081c196c6fd96d548a2
Deepak Jha <deepakjha14.ex@gmail.com>
2024-12-01 11:54:57 +0530

    Chapter 7 completes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { NgOptimizedImage } from "@angular/common";
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import * as fakeData from "../../public/sampleResponse.json";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NgOptimizedImage],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
