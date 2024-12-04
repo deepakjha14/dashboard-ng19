c7ff8b5521c4026d672390e1f41d950c58fb2ecb
Deepak Jha <deepakjha14.ex@gmail.com>
2024-12-04 18:20:58 +0530

    Forms, template and reactive both completes

--- src/about/about.component.ts
+++ src/about/about.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule, FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'dashboard-ng19-about',
-  imports: [],
+  imports: [ FormsModule, ReactiveFormsModule ],
   templateUrl: './about.component.html',
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  studentsDetails: FormGroup<any> = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    city: new FormControl(''),
+    age: new FormControl(''),
+  });
 
+  onClick() {
+    alert(JSON.stringify(this.studentsDetails?.value));
+  }
 }
--- src/home/home.component.ts
+++ src/home/home.component.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'dashboard-ng19-home',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  login: any = {
+    username: '',
+    password: ''
+  }
 
+  onClick() {
+    alert("User name is - "+this.login.username+" and password is - "+this.login.password);
+  }
 }
